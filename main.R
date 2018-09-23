2a4a4fc1fac4b222584fb1b8e729a41f24b5e87d
Muhammad Tehami <t3hami@gmail.com>
2018-09-23 22:59:13 +0500

    Initial commit.

---
+++ main.R
@@ -0,0 +1,166 @@
+train <- read.csv("titanic_train.csv", header = TRUE)
+test <- read.csv("titanic_test.csv", header = TRUE)
+
+# Add a "Survived" variable to the test set to allow for combining data sets
+test.survived <- data.frame(Survived = rep("None", nrow(test)), test[,])
+
+# Combine data sets
+data.combined <- rbind(train, test.survived)
+
+data.combined$Survived <- as.factor(data.combined$Survived)
+data.combined$Pclass <- as.factor(data.combined$Pclass)
+
+# Load up ggplot2 package to use for visualizations
+library(ggplot2)
+library(stringr)
+
+# Create a utility function to help with title extraction
+# NOTE - Using the grep function here, but could have used the str_detect function as well.
+extractTitle <- function(name) {
+  name <- as.character(name)
+  
+  if (length(grep("Miss.", name)) > 0) {
+    return ("Miss.")
+  } else if (length(grep("Master.", name)) > 0) {
+    return ("Master.")
+  } else if (length(grep("Mrs.", name)) > 0) {
+    return ("Mrs.")
+  } else if (length(grep("Mr.", name)) > 0) {
+    return ("Mr.")
+  }   else {
+    return ("Other")
+  }
+}
+
+
+# NOTE - The code below uses a for loop which is not a very R way of
+#        doing things
+titles <- NULL
+for (i in 1:nrow(data.combined)) {
+  titles <- c(titles, extractTitle(data.combined[i,"Name"]))
+}
+data.combined$title <- as.factor(titles)
+
+
+# Cabin really isn't a factor, make a string and the display first 100
+data.combined$cabin <- as.character(data.combined$Cabin)
+
+# Let's try some feature engineering. What about creating a family size feature?
+temp.sibsp <- c(train$SibSp, test$SibSp)
+temp.parch <- c(train$parch, test$Parch)
+data.combined$family.size <- as.factor(temp.sibsp + temp.parch + 1)
+
+
+
+#==============================================================================
+#
+# Exploratory Modeling using Random Forests
+#
+#==============================================================================
+
+
+library(randomForest)
+
+# Train a Random Forest with the default parameters using pclass & title
+rf.train.1 <- data.combined[1:891, c("Pclass", "title")]
+rf.label <- as.factor(train$Survived)
+
+set.seed(1234)
+rf.1 <- randomForest(x = rf.train.1, y = rf.label, importance = TRUE, ntree = 1000)
+rf.1
+varImpPlot(rf.1)
+
+
+
+# Train a Random Forest using pclass, title, & sibsp
+rf.train.2 <- data.combined[1:891, c("Pclass", "title", "SibSp")]
+
+set.seed(1234)
+rf.2 <- randomForest(x = rf.train.2, y = rf.label, importance = TRUE, ntree = 1000)
+rf.2
+varImpPlot(rf.2)
+
+
+
+# Train a Random Forest using pclass, title, & parch
+rf.train.3 <- data.combined[1:891, c("Pclass", "title", "Parch")]
+
+set.seed(1234)
+rf.3 <- randomForest(x = rf.train.3, y = rf.label, importance = TRUE, ntree = 1000)
+rf.3
+varImpPlot(rf.3)
+
+
+
+# Train a Random Forest using pclass, title, sibsp, parch
+rf.train.4 <- data.combined[1:891, c("Pclass", "title", "SibSp", "Parch")]
+
+set.seed(1234)
+rf.4 <- randomForest(x = rf.train.4, y = rf.label, importance = TRUE, ntree = 1000)
+rf.4
+varImpPlot(rf.4)
+
+
+
+# Train a Random Forest using pclass, title, & family.size
+rf.train.5 <- data.combined[1:891, c("Pclass", "title", "family.size")]
+
+set.seed(1234)
+rf.5 <- randomForest(x = rf.train.5, y = rf.label, importance = TRUE, ntree = 1000)
+rf.5
+varImpPlot(rf.5)
+
+
+
+# Train a Random Forest using pclass, title, sibsp, & family.size
+rf.train.6 <- data.combined[1:891, c("Pclass", "title", "SibSp", "family.size")]
+
+set.seed(1234)
+rf.6 <- randomForest(x = rf.train.6, y = rf.label, importance = TRUE, ntree = 1000)
+rf.6
+varImpPlot(rf.6)
+
+
+
+# Train a Random Forest using pclass, title, parch, & family.size
+rf.train.7 <- data.combined[1:891, c("Pclass", "title", "Parch", "family.size")]
+
+set.seed(1234)
+rf.7 <- randomForest(x = rf.train.7, y = rf.label, importance = TRUE, ntree = 1000)
+rf.7
+varImpPlot(rf.7)
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+#==============================================================================
+#
+# Predictions
+#
+#==============================================================================
+
+
+# Before we jump into features engineering we need to establish a methodology
+# for estimating our error rate on the test set (i.e., unseen data). This is
+# critical, for without this we are more likely to overfit. Let's start with a 
+# submission of rf.5 to Kaggle to see if our OOB error estimate is accurate.
+
+# Subset our test records and features
+test.submit.df <- data.combined[892:1309, c("Pclass", "title", "family.size")]
+
+# Make predictions
+rf.5.preds <- predict(rf.5, test.submit.df)
+table(rf.5.preds)
+
+# Write out a CSV file for submission to Kaggle
+submit.df <- data.frame(PassengerId = rep(892:1309), Survived = rf.5.preds)
+
+write.csv(submit.df, file = "RF_SUB_23092018_1.csv", row.names = FALSE)
+
+# Our submission scores 0.79426, but the OOB predicts that we should score 0.8159.
+# Let's look into cross-validation using the caret package to see if we can get
+# more accurate estimates
